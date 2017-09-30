7a262d0f939b4e45334aa5bccf3d383625d11803
Robert Kwan <Robert.Kwan@moneytech.com.au>
2017-09-30 13:31:32 +1000

    added backend and light frontend support for game room picker

--- rooms/game-room.ts
+++ rooms/game-room.ts
@@ -10,6 +10,7 @@ enum Action {
 interface RoomData {
   name: string,
   players: Array<string>,
+  readyPlayers: Array<string>,
   maxPlayers: number
 }
 
@@ -19,51 +20,92 @@ export class GameRoom extends Room {
     return arr.filter(room => room.name == name) > 0;
   }
 
+  private markPlayerReady(name: string) {
+    for(let i = 0; i < this.state.rooms.length; i++){
+      if(this.state.rooms[i].players.includes(name)){
+        this.state.rooms[i].readyPlayers.push(name);
+        break;
+      }
+    }
+  }
+
+  private leaveRoom(name: string) {
+    for(let i: number = 0; i < this.state.rooms.length; i++){
+      let index: number = this.state.rooms[i].players.indexOf(name);
+      let readyIndex: number = this.state.rooms[i].readyPlayers.indexOf(name);
+
+      if(readyIndex > -1){
+        this.state.rooms[i].readyPlayers.splice(readyIndex, 1);
+      }
+
+      if(index > -1){
+        this.state.rooms[i].players.splice(index, 1);
+        if(this.state.rooms[i].players.length == 0){
+          this.state.rooms.splice(i, 1);
+        }
+        break;
+      }
+    }
+  }
+
+  private joinRoom(room: string, player: string) {
+    for(let i = 0; i < this.state.rooms.length; i++){
+      if(this.state.rooms[i].roomName == room){
+        this.state.rooms[i].players.push(player);
+        break;
+      }
+    }
+  }
+
   onInit (options) {
       this.setState({
         rooms: [],
-        messages: [],
         players: [],
       });
       console.log("GameRoom created!", options);
   }
 
   onJoin (client: Client) {
-      this.state.messages.push(`${ client.id } joined.`);
-      this.state.players.push(client.id);
+    console.log("NEW CLIENT!", client);
+    this.state.players.push(client.id);
   }
 
   onLeave (client: Client) {
-      this.state.messages.push(`${ client.id } left.`);
       // this.state.players.push(client.id);
-      let index: number = this.state.messages.indexOf(client.id);
+      let index: number = this.state.players.indexOf(client.id);
       this.state.players.splice(index, 1);
   }
 
   onMessage (client: Client, data) {
       console.log("ChatRoom:", client.id, data);
 
-      let action: Action = data.action;
+      let action: string = data.action;
       switch (action) {
-        case Action.Create:
+        case "CREATE":
           console.log("make new room");
           let newRoom: RoomData = {
-            name: "room1",
-            maxPlayers: data.maxPlayers,
-            players: [ client.id ]
+            name: data.payload.roomName,
+            maxPlayers: data.payload.maxPlayers,
+            players: [ client.id ],
+            readyPlayers: []
           }
           
-          // while(this.roomNameExists(newRoom.name)){
-          //   newRoom.name = randomNameGenerator();
-          // }
-
-          this.state.rooms.push(newRoom);
+          if(!this.roomNameExists(data.payload.roomName)){
+            this.state.rooms.push(newRoom);
+          }
           break;
-        case Action.Join:
+        case "JOIN":
           console.log("Join existing room");
+          const roomName: string = data.payload.roomName;
+          this.joinRoom(roomName, client.id);
+          break;
+        case "READY":
+          console.log("PLAYER READY");
+          this.markPlayerReady(client.id);
           break;
         default:
-          console.log("Leaving current room")
+          console.log("Leaving current room");
+          this.leaveRoom(client.id);
           break;
       }
   }
