b4237c6a7a7a2130e9092bc4e231bf8b0e88e20b
Robert Kwan <Robert.Kwan@moneytech.com.au>
2017-10-07 12:57:39 +1100

    brought back euclidean distance

--- rooms/game-arena.ts
+++ rooms/game-arena.ts
@@ -7,14 +7,16 @@ interface PlayerInfo {
   team: string,
   health:number,
   data: {
-    position:{
-      x: number,
-      y: number,
-      z: number
-    },
+    position: Coords,
     moveAnimation:string
   }
   skillAnimation:string
+}
+
+interface Coords {
+  x: number,
+  y: number,
+  z: number
 }
 
 export class GameArena extends Room {
@@ -31,21 +33,25 @@ export class GameArena extends Room {
           z: 1
         },
         moveAnimation: "idle"
-    },
-    skillAnimation: "skill1"
-  };
+      },
+      skillAnimation: "skill1"
+    };
 
     let newPlayer2: PlayerInfo = {
       team: "red",
       character: "tank",
       health: 100,
-      position: {
-        x: 1.5,
-        y: 3.5,
-        z: 4
+      data: {
+        position: {
+          x: 1.5,
+          y: 3.5,
+          z: 4
+        },
+        moveAnimation: "idle"
       },
       skillAnimation: "skill1"
     };
+
     this.setState({
       players: <Array<PlayerInfo>> [newPlayer1, newPlayer2],
     });
@@ -77,23 +83,34 @@ export class GameArena extends Room {
 
   onMessage (client: Client, data) {
      // console.log("Game Arena:", client.id, data);
-      if(data.action == "idle"){
-        return;
-      }
-      if(data.action == "MOVE"){
-      console.log(data);
-      if(data.action== "MOVE"){
-        for (var index = 0; index < this.state.players.length; index++) {
-          var element = this.state.players[index];
-          if(element.id == client.id){
-            console.log(data.data)
-            this.state.players[index].data = data.data;
-          }
+    if(data.action == "idle"){
+      return;
+    }
+
+    console.log(data);
+    if(data.action== "MOVE"){
+      for (var index = 0; index < this.state.players.length; index++) {
+        var element = this.state.players[index];
+        if(element.id == client.id){
+          console.log(data.data)
+          this.state.players[index].data = data.data;
         }
       }
+    }
 
-      // this.messageClient(client);
+    // this.messageClient(client);
   }
+
+  euclideanDistance(a: Coords, b: Coords){ 
+    let sums = [ 
+      Math.pow(a.x - b.x, 2), 
+      Math.pow(a.y - b.y, 2), 
+      Math.pow(a.z - b.z, 2) 
+    ]; 
+    return Math.sqrt(sums.reduce(function(accumulator, currentValue) { 
+        return accumulator + currentValue; 
+    })); 
+  } 
 
   messageClient (client: Client) {
     let container = [];
