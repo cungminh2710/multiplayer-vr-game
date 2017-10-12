145c8993f187a0540ad0f8d2db1f75eb7074fe03
Minh Cung <z3493000@student.unsw.edu.au>
2017-10-12 16:54:05 +1100

    - Fix rookie Mongo mistake: _id instead of id

--- src/helper.ts
+++ src/helper.ts
@@ -17,8 +17,8 @@ export let createNewUser: {
  * Check if an user id is valid and existed
  * @param id User ID
  */
-export let isUserExist: { (id: string): Promise<boolean> } = id =>
-	User.count({ id }).exec();
+export let isUserExist: { (id: string): Promise<boolean> } = _id =>
+	User.count({ _id }).exec();
 
 /**
  * Update session ID of an user given his/her User ID
@@ -28,18 +28,40 @@ export let isUserExist: { (id: string): Promise<boolean> } = id =>
 
 export let updateUserSession: {
 	(id: string, sessionID: string): Promise<void>;
-} = (id, sessionID) => User.update({ id }, { sessionID }).exec();
+} = (_id, sessionID) => User.update({ _id }, { sessionID }).exec();
 
 /**
  * Read user data based on User ID
  * @param id User ID
  */
-export let readUser: { (id: string): Promise<IUserModel> } = id =>
-	User.findOne({ id }).exec();
+export let readUser: { (id: string): Promise<IUserModel> } = _id =>
+	User.findOne({ _id }).exec();
 
 /**
  * Read user stats based on User ID
  * @param id User ID
  */
-export let readUserStats: { (id: string): Promise<IUserStats> } = id =>
-	readUser(id).then(user => user.stats);
+export let readUserStats: { (id: string): Promise<IUserStats> } = _id =>
+	readUser(_id).then(user => Promise.resolve(user.stats));
+
+/**
+ * Update User Stats
+ * @param id User ID 
+ * @param newStats Updated User Stats
+ */
+export let updateUserStats: {
+	(id: string, newStats: IUserStats): Promise<void>;
+} = (_id, newStats) => User.update({ _id }, { stats: newStats }).exec();
+
+/**
+ * Get User ID from sessionID
+ * @param sessionID Session ID
+ */
+
+export let readUserIDBySession: {
+	(sessionID: string): Promise<String>;
+} = sessionID =>
+	User.find({ sessionID })
+		.select("_id")
+		.exec()
+		.then(user => Promise.resolve(user._id));
