ddf1b9a1ce4c8e77413e129625a82c2805a41248
Unknown <yaoyu5507203@gmail.com>
2017-10-19 21:00:22 +1100

    skill logic

--- static/vrGame/js/skills/healer.js
+++ static/vrGame/js/skills/healer.js
@@ -1,20 +1,61 @@
 class Healer{
-    
-        static attack(){
-            //gameRoom.send({action: "DAMAGE", data:"TODO"}); 
-            gameRoom.send({action: "SKILLANIMATION", data:"attack"});
-        
-        }
-    
-        static skill1(){
 
-            gameRoom.send({action: "SKILLANIMATION", data:"skill1"});
+    static attack(){
+        // do not need a target, release skill directly
+        Animation.setAnimation(el.children[0].querySelector("#"+client.id),"attack")
+        gameRoom.send({action: "SKILLANIMATION", data:"skill1"});
+        //find a enemy target 
+        var target = raycasterEl.components.raycaster.intersectedEls[0];
+        if(!target) return;
+        if(target.getAttribute("team") == "ally") return;
+        //send damage
+        var data = {
+            //not AOE
+            target:[target.getAttribute("id")],
+            name:"HealerAttack",
         }
-        
-        static skill2(){
-
-            gameRoom.send({action: "SKILLANIMATION", data:"skill2"});
-    
-        }
-    
+        console.log("SEND HealerAttack: ",data)
+        gameRoom.send({action: "DAMAGE", data}); 
     }
+    //cure
+    static skill1(){
+    //must need an ally target
+    var target = raycasterEl.components.raycaster.intersectedEls[0];
+    if(!target) return;
+    if(target.getAttribute("team") != "ally") return;
+    // realease skill and update cd
+    Animation.setAnimation(el.children[0].querySelector("#"+client.id),"skill1")
+    gameRoom.send({action: "SKILLANIMATION", data:"skill1"}); 
+    //send damage
+    var skill1 = panel.querySelector("#skill1");
+    var data = {
+        //not AOE
+        target:[target.getAttribute("id")],
+        name:"Cure",
+    }
+    console.log("SEND Cure: ",data)
+    gameRoom.send({action: "DAMAGE", data}); 
+    }
+    
+    static skill2(){
+        var ally = [];
+        Animation.setAnimation(el.children[0].querySelector("#"+client.id),"skill2")
+        gameRoom.send({action: "SKILLANIMATION", data:"skill2"});
+        for (var id in playersDict) {
+            // check if the property/key is defined in the object itself, not in parent
+            if (playersDict.hasOwnProperty(id)) {           
+                if(playersDict[id].getAttribute("team")=="ally") ally.push[id]
+            }
+        }
+        //send damage
+        var skill1 = panel.querySelector("#skill1");
+        var data = {
+            //AOE
+            target:ally,
+            name:"Wish",
+        }
+        console.log("SEND Wish: ",data)
+        gameRoom.send({action: "DAMAGE", data}); 
+        }
+    
+}
